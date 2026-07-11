7a355f7ed720f5b61da740a9dd5d6d27de3c3748
Daniel Nguyen <ducduy.ababat@gmail.com>
2026-07-11 13:05:52 +1000

    feat: wire git vault clean/smudge to the real vault

--- internal/cli/clean.go
+++ internal/cli/clean.go
@@ -1,19 +1,23 @@
 package cli
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
+
+	"github.com/ducduyn31/git-vault/internal/vault"
 )
 
 func newCleanCmd() *cobra.Command {
 	return &cobra.Command{
-		Use:    "clean [path]",
+		Use:    "clean <path>",
 		Short:  "Git clean filter entry point (encrypt on stage)",
-		Args:   cobra.MaximumNArgs(1),
+		Args:   cobra.ExactArgs(1),
 		Hidden: true,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return fmt.Errorf("git vault clean: not implemented in scaffold")
+			v, recipients, err := newLocalVault()
+			if err != nil {
+				return err
+			}
+			return v.SealStream(cmd.OutOrStdout(), cmd.InOrStdin(), vault.FormatForPath(args[0]), recipients)
 		},
 	}
 }
