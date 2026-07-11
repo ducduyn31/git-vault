c33988f1872d558bd7ec8314eb30c0506fbe0819
Daniel Nguyen <ducduy.ababat@gmail.com>
2026-07-12 04:43:09 +1000

    feat: render errors via ui.Error in main.go

--- cmd/git-vault/main.go
+++ cmd/git-vault/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/ducduyn31/git-vault/internal/cli"
+	"github.com/ducduyn31/git-vault/internal/ui"
 )
 
 func main() {
 	if err := cli.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		ui.Error(os.Stderr, err)
 		os.Exit(1)
 	}
 }
